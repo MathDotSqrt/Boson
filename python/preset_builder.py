78e741fbfce560ad319ae0ca066f28c02a59c2bb
MathDotSqrt <ctrenkov@gmail.com>
2020-07-28 09:48:54 -0400

    building python builder

--- python/preset_builder.py
+++ python/preset_builder.py
@@ -1,2 +1,12 @@
+DELIM = ','
+PLATFORM_FILENAME = '../data/platform.csv'
 
-print("TEST")
+def read_csv(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    columns = [line.strip().split(DELIM) for line in lines]
+    return columns;
+
+print(read_csv(PLATFORM_FILENAME)[:3])
