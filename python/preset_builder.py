c8b6908cbb6a08da3591f4753f08fa934144eeb9
MathDotSqrt <ctrenkov@gmail.com>
2020-07-29 09:01:58 -0400

    not done. gonna work on UI

--- python/preset_builder.py
+++ python/preset_builder.py
@@ -1,12 +1,36 @@
+import json
 DELIM = ','
 PLATFORM_FILENAME = '../data/platform.csv'
 
-def read_csv(filename):
+preset = {
+    "platform" : [{
+        "name" : "platform.csv",
+    }],
+    "schedule" : None,
+    "targets" : []
+}
+
+def read_file(filename):
     lines = []
     with open(filename, 'r') as f:
         lines = f.readlines()
+    return lines
 
+def read_json(filename):
+    lines = read_file(filename)
+def read_csv(filename):
+    lines = read_file(filename)
     columns = [line.strip().split(DELIM) for line in lines]
     return columns;
 
-print(read_csv(PLATFORM_FILENAME)[:3])
+def parse_platform(filename):
+    print(read_csv(filename))
+
+def import_platform(platform):
+    for p in platform:
+        parse_platform(p['name']);
+
+def import_preset(preset):
+    import_platform(preset["platform"])
+
+import_preset(preset)
