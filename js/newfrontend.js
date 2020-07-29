262f5cc79ff51715e1fdd3d75d33f8a35e8ae645
MathDotSqrt <ctrenkov@gmail.com>
2020-07-29 15:32:45 -0400

    can now click on the tabs

--- js/newfrontend.js
+++ js/newfrontend.js
@@ -1,16 +1,39 @@
 /* PANEL */
-export function openNav(){
+function initPanel(){
+  for(const element of document.getElementsByClassName("close_btn")){
+    element.onclick = closeNav;
+  }
+
+  for(const element of document.getElementsByClassName("open_btn")){
+    element.onclick = openNav;
+  }
+
+  const tabs = document.getElementsByClassName("selector");
+  for(const tab of tabs){
+    tab.onclick = () => selectTab(tab.id);
+  }
+}
+
+function openNav(){
   document.getElementById("side_panel_1").style.width = "285px";
 }
-export function closeNav(){
+function closeNav(){
   document.getElementById("side_panel_1").style.width = "0px";
 }
 
-for(const element of document.getElementsByClassName("close_btn")){
-  element.onclick = closeNav;
+function selectTab(id){
+  const SELECTED = "selected";
+
+  const tab = document.getElementById(id);
+  if(tab){
+    const tabs = document.getElementsByClassName("selector");
+    for(const tab of tabs){
+      tab.classList.remove(SELECTED);
+    }
+
+    tab.classList.add(SELECTED);
+  }
 }
 
-for(const element of document.getElementsByClassName("open_btn")){
-  element.onclick = openNav;
-}
+initPanel();
 /* PANEL */
