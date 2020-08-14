2fe5824d149adba558b7184c719c07c3308a871a
MathDotSqrt <ctrenkov@gmail.com>
2020-08-14 10:19:28 -0400

    builder

--- python/preset_builder.py
+++ python/preset_builder.py
@@ -1,14 +1,45 @@
 import json
+from os import listdir
+from os.path import isfile, join, basename
+from functools import reduce
+
 DELIM = ','
-PLATFORM_FILENAME = '../data/platform.csv'
+
+PLATFORM_COLUMN_MAP = {
+    "platformID" : "PlatformID",
+    "time" : "Time",
+    "posx" : "PositionX",
+    "posy" : "PositionY",
+    "posz" : "PositionZ",
+    "velx" : "VelocityX",
+    "vely" : "VelocityY",
+    "velz" : "VelocityZ",
+}
 
 preset = {
-    "platform" : [{
+    "platform" : {
         "name" : "platform.csv",
-    }],
+        "path" : "../data/platform.csv",
+        "satellites" : [
+            {
+                
+            }
+        ]
+    },
     "schedule" : None,
     "targets" : []
 }
+
+def listdir_fullpath(d):
+    return [join(d, f) for f in listdir(d)];
+
+def get_files_in_dir(foldername):
+    return [f for f in listdir_fullpath(foldername) if isfile(f)]
+
+def get_filenames(foldernames):
+    filenames = reduce(lambda a, b : a + b, map(get_files_in_dir, foldernames));
+    return filenames
+
 
 def read_file(filename):
     lines = []
@@ -23,14 +54,50 @@ def read_csv(filename):
     columns = [line.strip().split(DELIM) for line in lines]
     return columns;
 
-def parse_platform(filename):
-    print(read_csv(filename))
+def get_header_indices(header, column_map):
+    indices = [(k, header.index(v)) for [k, v] in column_map.items() if v in header];
+    if(len(indices) != len(column_map.values())):
+        return None
+    return dict(indices)
+
+def parse_platform(platform):
+    csv = read_csv(platform["path"]);
+    header = csv[0];
+    content = csv[1:];
+    index_map = get_header_indices(header, PLATFORM_COLUMN_MAP)
+
+    if(index_map == None):
+        return None
+
+    for row in content:
+        id = row[index_map["platformID"]];
+        print(id)
+    print(index_map);
+
 
 def import_platform(platform):
-    for p in platform:
-        parse_platform(p['name']);
+    parse_platform(platform);
 
 def import_preset(preset):
     import_platform(preset["platform"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 import_preset(preset)
